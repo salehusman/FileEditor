cda85f74499eac0bd6c8de9d4764849ce1aaf960
WithoutCaps <withoutcapsdev@gmail.com>
2017-08-17 22:24:45 +0300

    Fixed up translation

--- app/src/main/java/site/withoutcaps/fileeditor/FileEditor.kt
+++ app/src/main/java/site/withoutcaps/fileeditor/FileEditor.kt
@@ -1,14 +1,5 @@
 package site.withoutcaps.fileeditor
 
-import android.Manifest
-import android.app.Activity
-import android.content.Context
-import android.content.pm.PackageManager
-import android.os.Environment
-import android.support.v4.app.ActivityCompat
-import android.support.v4.content.ContextCompat
-import android.util.Log
-
 import java.io.BufferedReader
 import java.io.BufferedWriter
 import java.io.File
@@ -20,250 +11,140 @@ import java.io.InputStreamReader
 import java.util.ArrayList
 import java.util.Arrays
 
-class FileEditor(folderName: String, storage: Int, private val mContext: Context) {
-    private var mPath: File? = null
-
-
-    init {
-        setPath(folderName, storage)
-    }
-
-    /**
-     * Method that sets "default path" if you will.
-
-     * @param path    name of the file OR path to the subdir(ex: Pictures/AwsomePic.jpg).
-     * *
-     * @param storage APP_PRIVATE_STORAGE or INTERNAL_STORAGE or CUSTOM_PATH_STORAGE.
-     */
-    fun setPath(path: String, storage: Int) {
-        when (storage) {
-            APP_PRIVATE_STORAGE -> this.mPath = File(mContext.filesDir.toString() + File.separator + path)
-            INTERNAL_STORAGE -> this.mPath = Environment.getExternalStoragePublicDirectory(path)
-            CUSTOM_PATH_STORAGE -> this.mPath = File(path)
-        }
-        Log.d(TAG, "mPath: " + mPath!!)
-        this.mPath!!.mkdirs()
-    }
-
-    /**
-     *
-     *
-     * Simple method that asks for WRITE_EXTERNAL_STORAGE premmision. (Required for Android 6.0 (API Level 23) and up)
-     *
-     *
-     *
-     * NOTE: This method runs ASYNCRONOUSLY!!!!!!!!!!!!!! Check link below (2017-01-17 edit: it looks like this is really simple and dirty implementation of premmisions, quite a few extra lines required....)
-     *
-     * For more information see: [Requesting Permissions at Run Time](https://developer.android.com/training/permissions/requesting.html)
-     */
-    fun askForPremmisions() {
-        if (ContextCompat.checkSelfPermission(mContext, Manifest.permission.WRITE_EXTERNAL_STORAGE) !== PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(mContext as Activity,
-                    arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE),
-                    MY_PERMISSIONS_REQUEST_WRITE_EXTERNAL_STORAGE)
-        }
-    }
+object FileEditor {
 
     /**
      * Writes List of strings to a file. (Each string in separate line)
-
+     *
      * @param path      location of file.
-     * *
      * @param data      List that needs to be saved from this cruel world of RAM. :D
-     * *
      * @param overwrite if passed value is 'true' - file will get overwrited with specified data, appends otherwise.
      */
-    fun writeList(path: String, data: List<*>, overwrite: Boolean?) {
-        try {
-            val writer = BufferedWriter(FileWriter(mPath.toString() + File.separator + path, (!overwrite)!!))
+    fun writeList(path: String, data: List<*>, overwrite: Boolean) {
+            val writer = BufferedWriter(FileWriter(path, !overwrite))
             for (line in data)
                 writer.write(line.toString() + System.getProperty("line.separator"))
             writer.flush()
             writer.close()
-        } catch (e: FileNotFoundException) {
-            e.printStackTrace()
-        } catch (e: IOException) {
-            e.printStackTrace()
-        }
-
     }
 
     /**
      * @param path      location of file.
-     * *
      * @param data      String that needs to be saved from this cruel world of RAM. :D
-     * *
      * @param overwrite if passed value is 'true' - file will get overwrited with specified data, appends otherwise.
      */
-    fun writeString(path: String, overwrite: Boolean?, addNewLine: Boolean?, vararg data: String) {
-        try {
-            val writer = BufferedWriter(FileWriter(mPath.toString() + File.separator + path, (!overwrite)!!))
+    fun writeString(path: String, overwrite: Boolean, addNewLine: Boolean, vararg data: String) {
+            val writer = BufferedWriter(FileWriter(path, !overwrite))
             for (s in data)
                 writer.write(s + if (addNewLine) System.getProperty("line.separator") else "")
             writer.flush()
             writer.close()
-        } catch (e: FileNotFoundException) {
-            e.printStackTrace()
-        } catch (e: IOException) {
-            e.printStackTrace()
-        }
-
     }
 
     /**
      * Reads file and returns List with its contents.
-
      * @param path location of file.
-     * *
+     *
      * @return List with file contents.(one line = one list item)
-     * *
+     *
      * @see File
      */
     fun readList(path: String): List<String> {
         val fileContent = ArrayList<String>()
-        try {
-            val br = BufferedReader(InputStreamReader(FileInputStream(mPath.toString() + File.separator + path)))
-            var strLine: String? = null
-            while ((strLine = br.readLine()) != null)
+            val br = BufferedReader(InputStreamReader(FileInputStream(path)))
+            while (true) {
+                val strLine = br.readLine()
+                if (strLine == null) break
                 fileContent.add(strLine)
+            }
             br.close()
-        } catch (e: FileNotFoundException) {
-            e.printStackTrace()
-        } catch (e: IOException) {
-            e.printStackTrace()
-        }
-
         return fileContent
     }
 
     /**
      * Reads file and returns String with its contents.
-
+     *
      * @param path location of file.
-     * *
      * @return String with file contents.
-     * *
      * @see File
      */
     fun readString(path: String): String {
         val builder = StringBuilder()
-        try {
-            val br = BufferedReader(InputStreamReader(FileInputStream(mPath.toString() + File.separator + path)))
-            var strLine: String? = null
-            while ((strLine = br.readLine()) != null)
-                builder.append(strLine!! + System.getProperty("line.separator"))
+            val br = BufferedReader(InputStreamReader(FileInputStream(path)))
+            while (true) {
+                val strLine = br.readLine()
+                if (strLine == null) break
+                builder.append(strLine)
+                builder.append(System.getProperty("line.separator"))
+            }
             br.close()
-        } catch (e: FileNotFoundException) {
-            e.printStackTrace()
-        } catch (e: IOException) {
-            e.printStackTrace()
-        }
-
         return builder.toString()
     }
 
     /**
      * @param name name of file.
-     * *
      * @return True if file exists, false otherwise.
-     * *
      * @see File
      */
-    fun exists(name: String): Boolean {
-        return File(mPath, name).exists()
-    }
+    fun exists(path: String): Boolean = File(path).exists()
 
     /**
      * Creates empty file
-
+     *
      * @param path name of a file.
-     * *
      * @return true if the file has been created, false if it already exists.
-     * *
      * @see File
      */
     fun createEmptyFile(path: String): Boolean {
         if (!exists(path))
-            try {
-                return File(mPath, path).createNewFile()
-            } catch (e: IOException) {
-                e.printStackTrace()
-            }
-
+            return File(path).createNewFile()
         return false
     }
 
-    fun createIfDosentExist(fileName: String, data: List<String>?) {
-        if (!exists(fileName))
-            if (data != null)
-                writeList(fileName, data, true)
-            else
-                createEmptyFile(fileName)
+    fun createIfDosentExist(path: String, data: List<String>?) {
+        if (!exists(path) && data != null)
+            writeList(path, data, true)
     }
 
-    fun createIfDosentExist(fileName: String, vararg data: String) {
-        createIfDosentExist(fileName, Arrays.asList(*data))
-    }
+    fun createIfDosentExist(path: String, vararg data: String) = createIfDosentExist(path, Arrays.asList(*data))
+
 
     /**
      * Renames file
-
+     *
      * @param from old file name.
-     * *
      * @param to   new file name.
-     * *
      * @return true if the file has been renamed.
-     * *
      * @see File
      */
-    fun rename(from: String, to: String): Boolean {
-        return File(mPath, from).renameTo(File(mPath, to))
-    }
+    fun rename(from: String, to: String): Boolean = File(from).renameTo(File(to))
 
     /**
      * Returns a list of File objects.
-
+     *
      * @param path folder name, or empty string for current folder.
-     * *
      * @return the file/folder at the specified location.
-     * *
      * @see File
      */
-    fun getFileList(path: String): Array<File> {
-        return File(mPath, path).listFiles()
-    }
+    fun getFileList(path: String): Array<File> = File(path).listFiles()
+
 
     /**
      * Returns specified File object.
-
+     *
      * @param path location of file.
-     * *
      * @return the file/folder at the specified location.
-     * *
      * @see File
      */
-    fun getFile(path: String): File {
-        return File(mPath, path)
-    }
+    fun getFile(path: String): File = File(path)
+
 
     /**
      * Deletes a File or folder.
-
+     *
      * @param path location of file/folder.
-     * *
      * @return true if specified file was deleted, false otherwise.
-     * *
      * @see File
      */
-    fun delete(path: String): Boolean {
-        return File(mPath, path).delete()
-    }
-
-    companion object {
-        private val TAG = "FileEditor"
-        val MY_PERMISSIONS_REQUEST_WRITE_EXTERNAL_STORAGE = 0
-        val APP_PRIVATE_STORAGE = 1
-        val INTERNAL_STORAGE = 2
-        val CUSTOM_PATH_STORAGE = 3
-    }
+    fun delete(path: String): Boolean = File(path).delete()
 }
--- app/src/main/java/site/withoutcaps/fileeditor/MainActivity.kt
+++ app/src/main/java/site/withoutcaps/fileeditor/MainActivity.kt
@@ -10,6 +10,7 @@ import android.view.Menu
 import android.view.MenuItem
 import android.view.View
 import android.widget.Toast
+import android.widget.Toolbar
 
 import java.io.File
 import java.util.ArrayList
@@ -23,39 +24,37 @@ class MainActivity : AppCompatActivity() {
         setSupportActionBar(toolbar)
 
 
-        val editor = FileEditor("FileEditorTestFolder!!!!", FileEditor.INTERNAL_STORAGE, this)
         val list = ArrayList<String>()
         list.add("a")
         list.add("b")
         list.add("c")
         list.add("d")
 
-        editor.askForPremmisions()
 
         Toast.makeText(this, "Click FAB to see the action and check ADB console", Toast.LENGTH_LONG).show()
 
         val fab = findViewById(R.id.fab) as FloatingActionButton
         fab.setOnClickListener(View.OnClickListener {
             Toast.makeText(getApplicationContext(), "Click", Toast.LENGTH_SHORT).show()
-            editor.writeList("test.txt", list, true)
+            FileEditor.writeList("test.txt", list, true)
 
-            editor.writeList("writingTest.txt", list, false)
-            for (file in editor.getFileList("")) {
+            FileEditor.writeList("writingTest.txt", list, false)
+            for (file in FileEditor.getFileList("")) {
 
                 Log.d(TAG, "File: " + file.absolutePath)
-                for (s in editor.readList(file.name))
+                for (s in FileEditor.readList(file.name))
                     Log.d(TAG, "readList: " + s)
 
-                Log.d(TAG, "readString: " + editor.readString(file.name))
+                Log.d(TAG, "readString: " + FileEditor.readString(file.name))
 
             }
 
-            editor.createEmptyFile("file1.txt")
-            editor.createEmptyFile("file2.txt")
-            editor.createEmptyFile("file3.txt")
+            FileEditor.createEmptyFile("file1.txt")
+            FileEditor.createEmptyFile("file2.txt")
+            FileEditor.createEmptyFile("file3.txt")
 
-            editor.rename("file1.txt", "file1 - renamed.txt")
-            editor.delete("file3.txt")
+            FileEditor.rename("file1.txt", "file1 - renamed.txt")
+            FileEditor.delete("file3.txt")
         })
     }
 
