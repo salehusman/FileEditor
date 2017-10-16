ed4bd659c049fb532fbe5ead1819852fcbc91f8e
WithoutCaps <withoutcapsdev@gmail.com>
2017-10-16 23:30:27 +0300

    Updated whole project files

--- app/src/main/java/com/withoutcaps/fileeditor/MainActivity.kt
+++ app/src/main/java/com/withoutcaps/fileeditor/MainActivity.kt
@@ -1,37 +1,12 @@
-package site.withoutcaps.fileeditor
+package com.withoutcaps.fileeditor
 
-import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import android.widget.Toast
-import site.withoutcaps.lessonsschedule.FileEditor
-import java.io.File
-import java.util.*
+import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
     }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
