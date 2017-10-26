d99f45417f2ced9e6703796958c969afed8e8eb2
WithoutCaps <withoutcapsdev@gmail.com>
2017-10-26 21:19:43 +0300

    Added "inline"

--- app/src/main/java/com/withoutcaps/fileeditor/AsyncEditor.kt
+++ app/src/main/java/com/withoutcaps/fileeditor/AsyncEditor.kt
@@ -3,34 +3,32 @@ package site.withoutcaps.fileeditor
 import kotlinx.coroutines.experimental.CommonPool
 import kotlinx.coroutines.experimental.launch
 import site.withoutcaps.lessonsschedule.FileEditor
-import java.io.File
 
 /**
  * Wrapper class for FileEditor.class which adds async calls
  */
 object AsyncEditor {
-    private val pool = CommonPool
 
-    fun readString(path: String, onDone: (String) -> Unit) = launch(pool, block = { onDone.invoke(FileEditor.readString(path)) })
-    fun readList(path: String, onDone: (MutableList<String>) -> Unit) = launch(pool, block = { onDone.invoke(FileEditor.readList(path)) })
+    inline fun readString(path: String, crossinline onDone: (String) -> Unit) = launch(CommonPool, block = { onDone.invoke(FileEditor.readString(path)) })
+    inline fun readList(path: String, crossinline onDone: (MutableList<String>) -> Unit) = launch(CommonPool, block = { onDone.invoke(FileEditor.readList(path)) })
 
     //TODO: Make it so that it would accept "vararg" as "data" param
-    fun writeString(path: String, overwrite: Boolean = true, data: String, onDone: () -> Unit) {
-        launch(pool, block = {
+    inline fun writeString(path: String, overwrite: Boolean = true, data: String, crossinline onDone: () -> Unit) {
+        launch(CommonPool, block = {
             FileEditor.writeString(path, overwrite, data)
             onDone.invoke()
         })
     }
 
-    fun writeList(path: String, data: Collection<*>, overwrite: Boolean = true, onDone: () -> Unit) {
-        launch(pool, block = {
+    inline fun writeList(path: String, data: Collection<*>, overwrite: Boolean = true, crossinline onDone: () -> Unit) {
+        launch(CommonPool, block = {
             FileEditor.writeList(path, data, overwrite)
             onDone.invoke()
         })
     }
 
-    fun createIfDosentExist(path: String, onDone: (Boolean) -> Unit, data: Collection<*>? = null) = launch(pool, block = { onDone.invoke(FileEditor.createIfDosentExist(path, data)) })
-    fun createIfDosentExist(path: String, onDone: (Boolean) -> Unit, data: String) = launch(pool, block = { onDone.invoke(FileEditor.createIfDosentExist(path, data)) })
+    inline fun createIfDosentExist(path: String, crossinline onDone: (Boolean) -> Unit, data: Collection<*>? = null) = launch(CommonPool, block = { onDone.invoke(FileEditor.createIfDosentExist(path, data)) })
+    inline fun createIfDosentExist(path: String, crossinline onDone: (Boolean) -> Unit, data: String) = launch(CommonPool, block = { onDone.invoke(FileEditor.createIfDosentExist(path, data)) })
 
-    fun delete(src: String, onDone: (Boolean) -> Unit) = launch(pool, block = { onDone.invoke(FileEditor.delete(src)) })
+    inline fun delete(src: String, crossinline onDone: (Boolean) -> Unit) = launch(CommonPool, block = { onDone.invoke(FileEditor.delete(src)) })
 }
