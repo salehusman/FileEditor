1c52915d43925b4cfb8956d507860c974980b506
WithoutCaps <withoutcapsdev@gmail.com>
2017-10-26 21:16:03 +0300

    Removed couple methods

--- app/src/main/java/com/withoutcaps/fileeditor/AsyncEditor.kt
+++ app/src/main/java/com/withoutcaps/fileeditor/AsyncEditor.kt
@@ -8,7 +8,7 @@ import java.io.File
 /**
  * Wrapper class for FileEditor.class which adds async calls
  */
-object AsyncEditor {    //TODO: Remove unnecessary methods (after performance check)
+object AsyncEditor {
     private val pool = CommonPool
 
     fun readString(path: String, onDone: (String) -> Unit) = launch(pool, block = { onDone.invoke(FileEditor.readString(path)) })
@@ -32,13 +32,5 @@ object AsyncEditor {    //TODO: Remove unnecessary methods (after performance ch
     fun createIfDosentExist(path: String, onDone: (Boolean) -> Unit, data: Collection<*>? = null) = launch(pool, block = { onDone.invoke(FileEditor.createIfDosentExist(path, data)) })
     fun createIfDosentExist(path: String, onDone: (Boolean) -> Unit, data: String) = launch(pool, block = { onDone.invoke(FileEditor.createIfDosentExist(path, data)) })
 
-    fun copyFile(src: File, dst: File, onDone: () -> Unit) = launch(pool, block = { FileEditor.copyFile(src, dst); onDone.invoke() })
-    fun copyFile(src: String, dst: String, onDone: () -> Unit) = launch(pool, block = { FileEditor.copyFile(src, dst); onDone.invoke() })
-
-    fun moveFile(src: File, dst: File, onDone: () -> Unit) = launch(pool, block = { FileEditor.moveFile(src, dst); onDone.invoke() })
-    fun moveFile(src: String, dst: String, onDone: () -> Unit) = launch(pool, block = { FileEditor.moveFile(src, dst); onDone.invoke() })
-
     fun delete(src: String, onDone: (Boolean) -> Unit) = launch(pool, block = { onDone.invoke(FileEditor.delete(src)) })
-    fun rename(src: String, dst: String, onDone: (Boolean) -> Unit) = launch(pool, block = { onDone.invoke(FileEditor.rename(src, dst)) })
-    fun getFileList(src: String, onDone: (Array<File>) -> Unit) = launch(pool, block = { onDone.invoke(FileEditor.getFileList(src)) })
 }
